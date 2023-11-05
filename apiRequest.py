3d9853ac2d3c723825dad8fb57096ebd1bb1bd5c
Dario Pacule <dariopacule@gmail.com>
2023-11-05 05:52:41 +0200

    Local
    Rebase JSON

--- apiRequest.py
+++ apiRequest.py
@@ -1,14 +1,14 @@
 import requests
 
-def post(phone,message):
-  url = 'http://localhost:3000/system_gate_way'
-  data = {'phoneNumber': phone, 'content': message}
+def post(phone, message):
+    url = 'http://localhost:3000/system_gate_way'
+    data = {'phoneNumber': phone, 'content': message}
 
-  token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2OTMwMjYxMzYsImV4cCI6MTAzMzI5Mzk3MzYsInN1YiI6ImQ2MTY4ODk3LWI1MGMtNGE5Yy04ZTg3LWZmYjNlMmY1ZTBmNSJ9.1a9lMAmQjIEoVWUkAnNa46K-aJbSSmLbiFjfdjPlcoQ'
+    token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2OTMwMjYxMzYsImV4cCI6MTAzMzI5Mzk3MzYsInN1YiI6ImQ2MTY4ODk3LWI1MGMtNGE5Yy04ZTg3LWZmYjNlMmY1ZTBmNSJ9.1a9lMAmQjIEoVWUkAnNa46K-aJbSSmLbiFjfdjPlcoQ'
 
-  headers = {'Authorization': f'Bearer {token}'}
+    headers = {'Authorization': f'Bearer {token}'}
 
-  response = requests.post(url, data=data,headers=headers)
+    response = requests.post(url, data=data, headers=headers)
 
   if response.status_code == 200:
     # A requisição foi bem-sucedida
