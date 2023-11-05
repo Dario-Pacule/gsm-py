5c82048555d9cfa8d6ed751639dbe8267cfca875
Dario Pacule <dariopacule@gmail.com>
2023-11-05 05:51:41 +0200

    Request

--- apiRequest.py
+++ apiRequest.py
@@ -12,6 +12,6 @@ def post(phone,message):
 
   if response.status_code == 200:
     # A requisição foi bem-sucedida
-    print("Sucesso: ",response)  # Exibe o conteúdo da resposta
+    print("Sucesso: ",response.json())  # Exibe o conteúdo da resposta
   else:
-    print(f'A requisição falhou com o código de status {response}')
+    print(f'A requisição falhou com o código de status {response.json()}')
