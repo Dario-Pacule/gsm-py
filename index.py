21db8a817f5c5d4b727fd8f50bf4f5af3a3f329d
Dario Pacule <dariopacule@gmail.com>
2023-11-08 05:40:05 +0200

    read message

--- index.py
+++ index.py
@@ -46,6 +46,11 @@ def send_sms_worker():
 
 
 def message_check_loop():
+    msg = sim800l.read_next_message(all_msg=True)
+    sim800l.check_incoming()
+
+    print("MESSAGE: ", msg)
+    """
     index_id = 0
     while True:
         index_id += 1
@@ -63,6 +68,7 @@ def message_check_loop():
         else:
             print("Nenhuma menssagem encontrada: ", msg)
             index_id = 0
+    """
         
 if sim800l.is_registered():
     print("SIM is registered.")
