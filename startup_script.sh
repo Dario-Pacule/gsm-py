6b98064baec7ddad6c2e018d5d68b63649e92035
Dario Pacule <dariopacule@gmail.com>
2023-11-11 05:39:16 +0200

    local

---
+++ startup_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Caminho para o interpretador Python do ambiente virtual
+source /home/turbokone/Documents/gsm-py/bin/activate
+
+# Caminho para o seu script Python
+python /home/turbokone/Documents/gsm-py/index.py
+
